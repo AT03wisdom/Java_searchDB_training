7e621dbd4d97ee7929bb296650a6ff432b9fd796
AT03wisdom <atsuki_tanaka@icloud.com>
2025-05-11 02:49:44 +0900

    arraylist sort/binary search

--- src/ArraySearcherApp.java
+++ src/ArraySearcherApp.java
@@ -1,20 +1,23 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 import printAssist.*;
-import searchIntArray.*;
+import searchIntArray.*; // 3. & 4.
 
 public class ArraySearcherApp {
     public static void main(String[] args) throws Exception {
 
         int[] degrees = {2, 345, 89, 132, 44, 288, 160, 91, 357, 60};
-        ArrayList<int[]> listDegrees = new ArrayList<int[]>();
-        listDegrees.add(degrees);
+        ArrayList<Integer> listDegrees = new ArrayList<Integer>();
+        for (int i : degrees) {
+            listDegrees.add(i);
+        }
         
         System.out.println("並び替え・探索アルゴリズムの演習");
 
         // 1. 配列のソートと線形探索
-        System.err.println(Arrays.toString(degrees)); // ランダムなまま出力
+        System.out.println(Arrays.toString(degrees)); // ランダムなまま出力
         System.out.println(linearSearchIntArray(50, degrees));
         System.out.println(linearSearchIntArray(60, degrees));
 
@@ -23,14 +26,27 @@ public class ArraySearcherApp {
         PrintAssist.printIntArray(degrees); // 昇順に並び替えたものを出力
         // （降順はIntegerじゃないと行えない, IntegerでもStringでも第二引数にCollections.reverseOrder()でできる）
 
-        // 2. 配列の二分探索 - 基本基本
-        BinarySearch.binarySearchIntIndex(357, degrees);
-        BinarySearch.binarySearchIntIndex(0, degrees);
+        // // 3. 配列の二分探索 - 基本基本
+        // BinarySearch.binarySearchIntIndex(357, degrees);
+        // BinarySearch.binarySearchIntIndex(0, degrees);
 
-        // 3. 配列の二分探索 - 指定の数より大きい値のうちの最小値
-        BinarySearch.binarySearchIntLowerBound(0, degrees);
+        // 4. 配列の二分探索 - 指定の数より大きい値のうちの最小値
+        // BinarySearch.binarySearchIntLowerBound(0, degrees);
         BinarySearch.binarySearchIntLowerBound(257, degrees);
-        BinarySearch.binarySearchIntLowerBound(358, degrees);
+        // BinarySearch.binarySearchIntLowerBound(358, degrees);
+
+        // 5. ライブラリを使った降順ソートと二分探索
+        System.out.println("並び替え・探索アルゴリズムの演習 : ArrayList");
+        System.out.println(listDegrees);
+        System.out.println(Collections.binarySearch(listDegrees, 0));
+        System.out.println(listDegrees);
+
+        Collections.sort(listDegrees);
+        System.out.println(listDegrees);
+        System.out.println(Collections.binarySearch(listDegrees, 2));
+        System.out.println(Collections.binarySearch(listDegrees, 300));
+        System.out.println(Collections.binarySearch(listDegrees, 357));
+        System.out.println(Collections.binarySearch(listDegrees, 360));
     }
 
     // 線形探索法
